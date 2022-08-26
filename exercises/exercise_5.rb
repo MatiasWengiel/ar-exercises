9c27a6e30764c0afd32cb188e85b9070aa75c0b9
Matias Wengiel <matiaswengiel@gmail.com>
2022-08-26 11:25:55 -0500

    Calculate total revenue for all stores. Calculate average revenue per store. Count stores with revenue over 1M

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,9 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@total_revenue = Store.sum("annual_revenue")
+@average_revenue = Store.sum("annual_revenue") / Store.count
+@stores_with_revenue_over_1m = Store.where(annual_revenue: 1000000..).count
+puts "Annual Revenue For All Stores: #{@total_revenue}"
+puts "Average Revenue Per Store: #{@average_revenue}"
+puts "Stores With A Revenue Over 1M: #{@stores_with_revenue_over_1m}"
